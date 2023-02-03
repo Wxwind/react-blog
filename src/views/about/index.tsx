f2e9915158b7eb2b8d84d2034c0a72513d1c02bb
Wxwind <373903632@qq.com>
2023-02-03 10:15:38 +0800

    update readme

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.scss";
 import ResumeContent from "./components/ResumeContent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
-const resumeURL = "/fileServer/resume/resume.pdf";
+const resumeURL = "https://assets.wxwind.top/static/resume/resume.pdf";
 
 const About = () => {
   return (
